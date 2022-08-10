e6667b61b72a62784bb5965e05c324cede5b6f90
luk7715 <eulrikagywu5@gmail.com>
2022-08-09 22:47:56 -0700

    added output

---
+++ output.py
@@ -0,0 +1,50 @@
+# This file is a part of DUDI, the Fortran-95 implementation 
+# of the two-body model for dust dynamics
+# Version 1.0.0
+# This is free software. You can use and redistribute it 
+# under the terms of the GNU General Public License (http://www.gnu.org/licenses/)
+# If you do, please cite the following paper
+# Anastasiia Ershova and Jürgen Schmidt, 
+# Two-body model for the spatial distribution of dust ejected from
+# an atmosphereless body, 2021, A&A, 650, A186 
+# File: dataoutmod.f95
+# Description: The subroutines used to write the result to a text-file
+
+# Author: Anastasiia Ershova
+# E-mail: vveyzaa@gmail.com
+
+import const
+import variables as var
+import numpy as np
+
+
+def result_out(density,nt):
+  
+  f = open("PyPlumes/results/twobody_model_result.txt","a") 
+  for i  in range(0, nt):
+    f.write(density[i,:] + " " + density[i,:] + " " + var.point.r + \
+       " " + (const.halfpi - var.point.alpha[i]) * const.rad2deg \
+        + " " + var.point.beta[i]*const.rad2deg)
+    f.write("\n")
+  f.close()
+
+
+
+# # end def result_out
+
+
+
+def cassini_flyby_out(density, ttab, bg, nt):
+  #integer, intent(in) :: nt
+  #real, intent(in) :: density(nt,2), ttab(nt), bg
+  #integer i
+  
+  f = open("PyPlumes/results/E2_profile.txt","a")
+  for i  in range(0, nt):
+    densitysum = np.sum (density[i,:]) + bg  
+    f.write(ttab[i] + " " + densitysum + "\n")
+  f.close 
+# # end def cassini_flyby_out  
+
+
+
---
+++ testing/enceladus_test.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+import const 
+import variables as var
+import integrator
+import input
+import output
+
+# number of sources
+Ns = 1
+# number of points on the SC trajectory for which the number density is to be calculated
+nt = 100
+tnow = 0.0
+bg = 0.01
+fname = "PyPlumes/input_data_files/Enceladus_jet.dat"
+density = np.zeros([nt,2])
+tmp_res = np.zeros([nt,2])
+
+var.source = input.read_sources_params(fname,Ns)
+ttab, var.point = input.read_Cassini_E2(nt)
+
+#print (var.source.r)
+
+for i in range (0, Ns):
+    for x in range (0,nt): 
+        var.point.r = var.point.r[x]; #var.point.alpha = var.point.alpha[x]
+        var.point.beta = var.point.beta[x]; var.point.r_scaled = var.point.r_scaled[x]
+        var.point.rvector = var.point.rvector[:,x]
+        
+        #print("rvector is " + str(var.point.rvector))
+        #print("r is " + str(var.point.r))
+        #var.source = var.source[i]
+        tmp_res[x,:] = integrator.DUDI(tnow)
+    density = density + tmp_res
+
+output.cassini_flyby_out(density, ttab, bg, nt)
